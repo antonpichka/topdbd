b5295325494439c5eacfcf0512de4084e3501fd6
antonpichka <antonpichka@gmail.com>
2023-08-25 20:26:32 +0300

    refactoring(.): r

--- common_topdbd/lib/model/discord_user/discord_user.dart
+++ common_topdbd/lib/model/discord_user/discord_user.dart
@@ -1,11 +1,13 @@
 import 'package:library_architecture_mvvm_modify/base_model/base_model.dart';
 
 base class DiscordUser extends BaseModel {
+  final String? id;
   final String? username;
   final String? globalName;
 
-  DiscordUser.success(super.uniqueId,this.username,this.globalName) : super.success();
+  DiscordUser.success(this.id,this.username,this.globalName) : super.success(id);
   DiscordUser.exception(super.exception) :
+        id = null,
         username = null,
         globalName = null,
         super.exception();
